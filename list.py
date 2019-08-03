89d6a79b6218268e4b7793b9bad4d88ab87f4744
Ryoung27 <RRYoung89@gmail.com>
2019-08-03 07:59:18 -0500

    data structs

---
+++ list.py
@@ -0,0 +1,34 @@
+# #List
+
+# node_1 = "Custom Object"
+
+# my_data_type = [1,2, False, 4, "Young", None, node_1, 5.0, 1]
+# print(my_data_type)
+# print(type(my_data_type))
+
+# #Dict
+
+# my_data_type = {1: "hello", 2: "Young"}
+# print(my_data_type[2])
+# print(type(my_data_type))
+
+# #Sets
+# my_data_type = {1,2, False, 4, "Young", None, node_1, 5.0, 1}
+# print(my_data_type)
+# print(type(my_data_type))
+
+# #Tuples
+# my_data_type = (1,2, False, 4, "Young", None, node_1, 5.0, 1)
+# print(my_data_type)
+# print(type(my_data_type))
+
+#Lists
+my_list = [15, 6, 7, 8, 35, 12, 14, 4, 10]
+my_strings_list = ["comp sci", "history", "physics", "law"]
+print(f"Ints: {my_list}")
+print(f"Strings: {my_strings_list}")
+print("Sorting...")
+my_list.sort()
+sorted_list = sorted(my_list)
+print(sorted_list)
+print(f"Sorted Ints: {my_list}")
